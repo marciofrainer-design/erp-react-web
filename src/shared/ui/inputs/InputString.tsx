23ebb1dcfe7453b1885146d3daedb309cedbd3aa
marcio frainer <marciofrainer@hotmail.com>
2026-03-26 11:48:09 -0300

    feat: initial commit

---
+++ src/shared/ui/inputs/InputString.tsx
@@ -0,0 +1,5 @@
+import { Field } from "@/components/ui/field";
+
+const InputString = ({ value, onChange }: { value: string; onChange: (value: string) => void }) => {
+  return (
+    <Field></Field>
